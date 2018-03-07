13356b257633916e48c8022fa1705baf169a6193
Lisa Gee <zhusufeng@gmail.com>
2018-03-07 10:58:46 -0800

    Wireframe out basic structure of App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,22 @@ import React, { Component } from 'react';
 class App extends Component {
   render () {
     return (
-      <div>Music Master from App</div>
+      <div>
+        <div className="App-title">
+          Music Master from App
+        </div>
+        <div>
+          <input placeholder="search an artist..." />
+          <button>Submit</button>
+        </div>
+        <div className="Profile">
+          <div>Artist Picture</div>
+          <div>Artist Name</div>
+        </div>
+        <div className="Gallery">
+          Gallery
+        </div>
+      </div>
     );
   }
 }
