f8a7f1a7b68e0f25e0d4a5d0d84dddd87868d117
Lisa Gee <zhusufeng@gmail.com>
2018-03-08 12:06:24 -0800

    Add props & map over props to render tracks in Gallery component

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -4,9 +4,31 @@ import './App.css';
 class Gallery extends Component {
   render () {
     console.log('gallery props: ', this.props);
+    const { tracks } = this.props;
+    console.log('tracks:', tracks);
     return (
       <div>
-        Gallery!
+        {
+          tracks.map((track, k) => {
+            const trackImg = track.album.images[0].url;
+
+            return (
+              <div 
+                key={k} 
+                className="track"
+              >
+                <img 
+                  src={trackImg}
+                  className="track-img"
+                  alt="Track"
+                />
+                <p className="track-text">
+                  {track.name}
+                </p>
+              </div>
+            );
+          })
+        }
       </div>
     );
   }
