2521141429b5c876c7300b3156a96c00ff32c7b0
Lisa Gee <zhusufeng@gmail.com>
2018-03-08 10:18:56 -0800

    Import spotify.config & create fetch to Spotify with token

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 import './App.css';
+import token from './spotify.config';
 
 class App extends Component {
   constructor (props) {
@@ -13,9 +14,19 @@ class App extends Component {
   search () {
     console.log('this.state', this.state);
     const BASE_URL = 'https://api.spotify.com/v1/search';
-    const q = this.state.query;
-    const FETCH_URL = `${BASE_URL}?q=${q}&type=artist&limit=1`;
-    console.log(FETCH_URL);
+    const FETCH_URL = `${BASE_URL}?q=${this.state.query}&type=artist&limit=1`;
+    // const myHeaders = new Headers();
+
+    fetch(FETCH_URL, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }, 
+      mode: 'cors',
+      cache: 'default'
+    })
+    .then(res => res.json())
+    .then(json => console.log(json));
   }
 
   render () {
