a332ac549b823cde0bd2ccf83a709dc97a771640
Lisa Gee <zhusufeng@gmail.com>
2018-03-07 21:54:20 -0800

    Add state, capture input as query in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,17 @@ import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      query: ''
+    };
+  }
+
+  search () {
+    console.log('this.state', this.state);
+  }
+
   render () {
     return (
       <div className="App">
@@ -14,8 +25,15 @@ class App extends Component {
             <FormControl 
               type="text"
               placeholder="Search for an Artist"
+              value={this.state.query}
+              onChange={(e) => { this.setState({ query: e.target.value }); }}
+              onKeyPress={(e) => {
+                if (e.key === 'Enter') {
+                  this.search();
+                }
+              }}
             />
-            <InputGroup.Addon>
+            <InputGroup.Addon onClick={() => this.search()}>
               <Glyphicon glyph="search"></Glyphicon>
             </InputGroup.Addon>
           </InputGroup>
